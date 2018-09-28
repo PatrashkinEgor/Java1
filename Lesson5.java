00eedf4d30501d8ffd5a65822c311ba13cd51004
eapat <eapat@mail.ru>
2018-09-28 14:10:57 +0500

    Добавлена проверка Try Catch для приема Int; Добавлен метод printInfo в класс Person; Дописаны комменты.

--- Lesson5.java
+++ Lesson5.java
@@ -4,6 +4,24 @@
  * @author Egor Patrashkin
  * @version dated Sep 28, 2018
  */
+
+
+
+
+/**Исходное задание:
+ * Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст;
+ * Конструктор класса должен заполнять эти поля при создании объекта;
+ * Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль;
+ * Создать массив из 5 сотрудников
+ * С помощью цикла вывести информацию только о сотрудниках старше 40 лет;
+ *
+ * Модернизация:
+ * Создается массив из N сотрудников, N запрашивается у пользователя;
+ * Данные о сотрудниках заполняются из консоли;
+ * Добавлена проверка на правильность заполнения полей;
+ * Возраст отбора сотрудников задается пользователем.
+ * */
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Lesson5 {
@@ -13,21 +31,28 @@ public class Lesson5 {
         System.out.println("Enter the number of employees");
         numberOfEmployees = sc.nextInt();
         Person[] persArray = new Person[numberOfEmployees];
-        for (int i = 0; i < numberOfEmployees ; i++) {
+
+        for (int i = 0; i < numberOfEmployees ; i++) {  //Создаем массив из объектов класса Person
             System.out.print("Enter the information about employee №" + (i+1) +": ");
             persArray[i] = new Person();
         }
-        System.out.print("Enter the age of the selection: ");
-        byte borderAge = sc.nextByte();
-        for (Person i: persArray) {
+
+
+        byte borderAge;
+        while(true){    //Получаем возраст отбора
+            try{
+                System.out.print("Enter the age of the selection: ");
+                borderAge = sc.nextByte();
+                break;
+            } catch (InputMismatchException ex){
+                System.out.println( "Incorrect input format!");
+                sc.next();
+            }
+        }
+
+        for (Person i: persArray) { //Выводим массив
             if (i.getAge()>borderAge){
-                System.out.println("Name: " + i.getName());
-                System.out.println("Age: " + i.getAge());
-                System.out.println("Function: " + i.getFunction());
-                System.out.println("Salary: " + i.getSalary());
-                System.out.println("Phone: " + i.getPhone());
-                System.out.println("Email: " + i.getEmail());
-                System.out.println();
+                i.printInfo();
             }
         }
     }
@@ -45,13 +70,30 @@ class Person{
     private int     salary;
     private byte    age;
 
+    /**
+     * Метод, который выводит информацию об объекте в консоль
+     * */
+    public void printInfo(){
+        System.out.println(); //Разделитель для удобного чтения списка
+        System.out.println("Name: " + this.getName());
+        System.out.println("Age: " + this.getAge());
+        System.out.println("Function: " + this.getFunction());
+        System.out.println("Salary: " + this.getSalary());
+        System.out.println("Phone: " + this.getPhone());
+        System.out.println("Email: " + this.getEmail());
+        System.out.println(); //Разделитель для удобного чтения списка
+    }
+
+    /**
+     * Конструктор класса с вводом с консоли
+     * */
     Person(){
         Scanner sc = new Scanner(System.in);
         while(true) {
             System.out.print("Enter the name: ");
             String name = sc.nextLine();
             if (isStringContainsANumber(name)) {
-                    System.out.println( "Incorrect input format!");
+                System.out.println( "Incorrect input format!");
             } else {
                 this.name = name;
                 break;
@@ -79,33 +121,48 @@ class Person{
         }
         while(true) {
             System.out.print("Enter the phone number:  ");
-            long phone = sc.nextLong();
-            if ((phone<1000)||(phone>99999999999l)) { //Принимаем что телефонный номер должен содержать от 4 до 11 разрядов
-                // Похорошему надо ловить эксепшены при выходе за формат, но я еще не умею.
+
+            try {
+                long phone = sc.nextLong();
+                if ((phone < 1000) || (phone > 99999999999l)) { //Принимаем что телефонный номер должен содержать от 4 до 11 разрядов
+                    System.out.println("Incorrect input format!");
+                } else {
+                    this.phone = phone;
+                    break;
+                }
+            } catch (InputMismatchException ex){
                 System.out.println( "Incorrect input format!");
-            } else {
-                this.phone = phone;
-                break;
+                sc.next();
             }
         }
         while(true) {
             System.out.print("Enter the salary: ");
-            int salary = sc.nextInt();
-            if (salary<100) { //Принимаем что зарплата не может быть меньше 100 условных едениц
+            try {
+                int salary = sc.nextInt();
+                if (salary < 100) { //Принимаем что зарплата не может быть меньше 100 условных едениц
+                    System.out.println("Incorrect input format!");
+                } else {
+                    this.salary = salary;
+                    break;
+                }
+            } catch (InputMismatchException ex){
                 System.out.println( "Incorrect input format!");
-            } else {
-                this.salary = salary;
-                break;
+                sc.next();
             }
         }
         while(true) {
             System.out.print("Enter the age: ");
-            byte age = sc.nextByte();
-            if ((age<14)||(age>100)) { //Принимаем что возраст не может быть больше 100 лет и меньше 14.
+            try {
+                byte age = sc.nextByte();
+                if ((age < 14) || (age > 100)) { //Принимаем что возраст не может быть больше 100 лет и меньше 14.
+                    System.out.println("Incorrect input format!");
+                } else {
+                    this.age = age;
+                    break;
+                }
+            } catch (InputMismatchException ex){
                 System.out.println( "Incorrect input format!");
-            } else {
-                this.age = age;
-                break;
+                sc.next();
             }
         }
     }
@@ -134,6 +191,9 @@ class Person{
         return phone;
     }
 
+    /**
+     * Метод проверяющий наличие цифры в строке(сделано "в лоб")
+     */
     private boolean  isStringContainsANumber(String st){
         if (st.contains("0")||st.contains("1")||st.contains("2")||st.contains("3")||
                   st.contains("4")||st.contains("5")||st.contains("6")||st.contains("7")||
