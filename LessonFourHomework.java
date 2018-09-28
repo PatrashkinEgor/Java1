8cf0614fa189a3ca4b7f629fc612f88e79340844
eapat <eapat@mail.ru>
2018-09-28 14:50:36 +0500

    Добавлен "Искусственный интеллект"; Дописаны комменты; Изменено имя класса, теперь соответствует имени файла.

--- LessonFourHomework.java
+++ LessonFourHomework.java
@@ -2,17 +2,24 @@
  * Java. Level 1. Lesson 4. Homework
  *
  * @author Egor Patrashkin
- * @version dated Sep 24, 2018
+ * @version dated Sep 28, 2018
  */
 
 
 import java.util.Random;
 import java.util.Scanner;
 
-class TicTacToe {
+/**
+ * Задание:
+ * 1. Полностью разобраться с кодом;
+ * 2. Переделать проверку победы, чтобы она не была реализована просто набором условий, например, с использованием циклов.
+ * 3. * Попробовать переписать логику проверки победы, чтобы она работала для поля любого размера и любого количества фишек.;
+ * 4. *** Доработать искусственный интеллект, чтобы он мог блокировать ходы игрока.
+ */
+class LessonFourHomework {
 
     final int SIZE = 10;
-    final int WINLINE = 2;      //Размер необходимой последовательности
+    final int WINLINE = 3;              //Размер необходимой последовательности
     final char DOT_X = 'x';
     final char DOT_O = 'o';
     final char DOT_EMPTY = '.';
@@ -21,7 +28,7 @@ class TicTacToe {
     Random rand = new Random();
 
     public static void main(String[] args) {
-        new TicTacToe().game();
+        new LessonFourHomework().game();
     }
 
     void game() {
@@ -36,8 +43,6 @@ class TicTacToe {
                 System.out.println("Sorry, DRAW...");
                 break;
             }
-
-
             if (aiTurn()) {
                 System.out.println("AI WON!");
                 break;
@@ -70,7 +75,7 @@ class TicTacToe {
         // результат хода(выиграл или нет)
         int x, y;
         do {
-            System.out.println("Enter X and Y (1..3):");
+            System.out.println("Enter X and Y (1.." + SIZE +"):");
             x = sc.nextInt() - 1;
             y = sc.nextInt() - 1;
         } while (!isCellValid(x, y));
@@ -80,17 +85,32 @@ class TicTacToe {
 
     boolean aiTurn() {
         int x, y;
+        //Сначала выберем точку "по умолчанию" чтобы компилятор не ругался на возможно не проинициализированные x и y
         do {
             x = rand.nextInt(SIZE);
             y = rand.nextInt(SIZE);
         } while (!isCellValid(x, y));
+        //Пробегаем по полю в поисках точки которая позволит X победить, если есть, то блокируем ее.
+        //Если не найдем такую точку, то О встанет в произвольную точку выбранную выше.
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (isCellValid(i,j)){
+                    if (checkWin(i,j, DOT_X)){
+                        x = i;
+                        y = j;
+                        break;
+                    }
+                }
+            }
+        }
         map[y][x] = DOT_O;
         printMap();
         return checkWin(x,y, DOT_O);
     }
 
     boolean checkWin(int x, int y,char dot) {
-
+        // Проверяем наоичие таких же точек по соседству в разных направлениях и считаем длину непрерывной линии.
+        // Если больше уставки, то выиграл.
         if ((getNumberOfNeighbors(x,y, -1,0,dot) + 1 + getNumberOfNeighbors(x,y, 1,0,dot))>= WINLINE){
             return true;
         }
@@ -119,6 +139,15 @@ class TicTacToe {
         */
     }
 
+    /**
+     * Метод возвращает длину линии из точек типа dot в заданном направлении из заданных координат
+     * @param x начальная координата точки по x
+     * @param y начальная координата точки по y
+     * @param vx направление поиска по x
+     * @param vy направление поиска по y
+     * @param dot Тип точки(X или O)
+     * @return
+     */
     int getNumberOfNeighbors(int x, int y, int vx, int vy, char dot) {
         int i = 0;
         x = x+vx;
